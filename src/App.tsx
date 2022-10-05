1e102bbb0142b5f739f25b4a10a8d85d5bdca965
William <wimakee@gmail.com>
2022-10-05 10:21:57 -0400

    feat: add prettier and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
+import 'weather-react-icons/lib/css/weather-icons.css';
 
-import logo from './logo.svg';
-import './App.scss';
+import { LocationContextProvider } from './providers/LocationProvider';
+import { LocationInput } from './containers/LocationInput';
+import { WeatherCard } from './containers/WeatherCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocationContextProvider>
+      <div className="flex flex-col items-center mt-20">
+        <LocationInput />
+        <WeatherCard />
+      </div>
+    </LocationContextProvider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,59 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/**/*.{js,jsx,ts,tsx}',
+  ],
+  prefix: '',
+  important: false,
+  separator: ':',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        light: '#F7f8f9',
+        dark: '#181818',
+      },
+      borderRadius: {
+        none: '0',
+        sm: '0.125rem',
+        default: '0.25rem',
+        md: '0.375rem',
+        lg: '0.5rem',
+        xl: '0.75rem',
+        '2xl': '1rem',
+        full: '2rem',
+      },
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"SpaceGrotesk"',
+          '"Lato"',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+        mono: [
+          'Menlo',
+          'Monaco',
+          'Consolas',
+          '"Liberation Mono"',
+          '"Courier New"',
+          'monospace',
+        ],
+      },
+    },
   },
-  plugins: [],
 };
